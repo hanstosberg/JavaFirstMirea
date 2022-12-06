a93454126743bb3991e10cc11ab4b32506807756
Артём Голомышкин <hanstosberg@gmail.com>
2022-12-06 11:57:19 +0300

    add

---
+++ src/ru/mirea/task29/Main.java
@@ -0,0 +1,25 @@
+package ru.mirea.task29;
+
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Creating obj
+        CurrencyInfo myCurrency = new CurrencyInfo(17);
+        // Serialize
+        FileOutputStream outputStream = new FileOutputStream("C:\\Users\\23906\\Desktop\\Java\\src\\ru\\mirea\\task29\\save.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        // Saving to file
+        objectOutputStream.writeObject(myCurrency);
+        // Closing
+        objectOutputStream.close();
+        // Deserialize
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\23906\\Desktop\\Java\\src\\ru\\mirea\\task29\\save.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        // Reading
+        CurrencyInfo myCurrencyDeserialized = (CurrencyInfo) objectInputStream.readObject();
+        // Printing
+        System.out.println(myCurrencyDeserialized);
+    }
+}
